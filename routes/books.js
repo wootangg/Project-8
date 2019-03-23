9290457298fc91b96f4247ad3dadfea45c730be4
wootangg <wjckorea001@gmail.com>
2019-03-23 22:08:29 +0900

    added update and delete

--- routes/books.js
+++ routes/books.js
@@ -11,24 +11,40 @@ router.get('/', function(req, res, next) {
 
 /* GET create new Book */
 router.get('/new', function(req, res, next) {
-  res.render('new-book', {book: Books.build() ,title: 'Books'});
+  res.render('new-book', {book: Books.build() });
 });
 
 /* POST create new Book */
-router.post('/', function(req, res, next) {
-  Books.create(req.body).then(function(book){
-    res.redirect('/books');
+router.post('/new', function(req, res, next) {
+  Books.create(req.body).then(function(){
+    res.redirect('/');
   });
 });
 
 /* GET individual book */
 router.get('/:id', function(req, res, next) {
   Books.findByPk(req.params.id).then(function(book){
-    res.render('update-book', {book: book, title: book.title});
+    res.render('update-book', {book: book, title: book.title, id: req.params.id});
   });
-
 });
 
+/* POST update individual book */
+router.post('/:id/update', function(req, res, next) {
+  Books.findByPk(req.params.id).then(function(book){
+    return book.update(req.body);
+  }).then(function() {
+    res.redirect('/');
+  });
+});
+
+/* POST delete individual book */
+router.post('/:id/delete', function(req, res, next) {
+  Books.findByPk(req.params.id).then(function(book){
+    return book.destroy();
+  }).then(function() {
+    res.redirect('/');
+  });
+});
 
 
 
